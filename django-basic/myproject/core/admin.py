ebd570ed609e939b262c9bc74ee73a582e3020a8
rg3915 <rg3915@yahoo.com.br>
2017-04-21 11:58:55 -0300

    Configurando o Admin

--- django-basic/myproject/core/admin.py
+++ django-basic/myproject/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Distributor, Category, Movie
 
-# Register your models here.
+admin.site.register(Distributor)
+admin.site.register(Category)
+admin.site.register(Movie)
